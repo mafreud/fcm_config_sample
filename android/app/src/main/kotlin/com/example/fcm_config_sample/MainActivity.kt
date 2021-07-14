b3faa51f25e2b4e78929a79ef5e40aca56ddb558
Masahiro Aoki <rafe.kun@gmail.com>
2021-07-14 15:28:10 +0900

    initial commit

---
+++ android/app/src/main/kotlin/com/example/fcm_config_sample/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fcm_config_sample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
